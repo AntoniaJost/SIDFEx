0ad6e3b9774bc99fb673a3f95fb5931021c536d6
AntoniaJost <antonia.jost@web.de>
2022-08-30 12:25:04 +0200

    loading reduced observations

--- R/sidfex.download.obs.R
+++ R/sidfex.download.obs.R
@@ -66,9 +66,12 @@ sidfex.download.obs <- function(index=NULL,TargetID=NULL,data.path=NULL,baseurl=
     i = i + 1
     if (is.null(baseurl)) {
       if (substr(tid, start=1, stop=5) %in% c("FIXED","POLAR","DISTN","CENTR")) {
+        # loading reduced (instead of full resoultion) observations
         baseurlx = "https://swift.dkrz.de/v1/dkrz_0262ea1f00e34439850f3f1d71817205/SIDFEx_index/observations_reduced/"
       } else {
-        baseurlx = "http://iabp.apl.washington.edu/WebData/"
+        # baseurlx = "http://iabp.apl.washington.edu/WebData/"
+        # changing baseurls to reduced observations data set from swiftbrowser for purpose of shorter runtime 
+        baseurlx = "https://swift.dkrz.de/v1/dkrz_0262ea1f00e34439850f3f1d71817205/SIDFEx_index/observations_reduced/"
       }
     } else if (length(baseurl) > 1) {
       baseurlx = baseurl[i]
